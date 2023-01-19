ad7c645e627553d67fa76de02e24c28a38a1f0f6
Evaristo Perez <perezlopezevaristo07@outlook.com>
2023-01-19 18:06:35 -0500

    Starting test

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button v-show="loggedIn">LogOut</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loggedIn: true,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ tests/unit/AppHeader.spec.js
@@ -0,0 +1,11 @@
+import AppHeader from "@/components/AppHeader.vue";
+describe("AppHeader", () => {
+  test("Si el usuario no está authenticado, no mostrar el boton de salir", () => {
+    expect(true).toBe(true);
+  });
+});
+describe("AppHeader", () => {
+  test("Si el usuario está authenticado, mostrar el boton de salir", () => {
+    expect(true).toBe(true);
+  });
+});
